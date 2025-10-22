c8549188abcbf3cf4fa825f601ad465c9db4e158
ST10479724-TrushenNaidoo <ST10479724@vcconnect.edu.za>
2025-10-22 16:17:33 +0200

    Type.tsx referencing added

--- Type.tsx
+++ Type.tsx
@@ -16,3 +16,36 @@ export type RootStackParamList = {
     setItems: React.Dispatch<React.SetStateAction<ChefC[]>>;
   };
 };
+
+
+// REFERENCES
+
+// Author: W3Schools
+// Version: Latest (2025)
+// Title: JavaScript Arrays
+// Available At: https://www.w3schools.com/js/js_arrays.asp
+
+// Author: W3Schools
+// Version: Latest (2025)
+// Title: JavaScript Data Types
+// Available At: https://www.w3schools.com/js/js_datatypes.asp
+
+// Author: W3Schools
+// Version: Latest (2025)
+// Title: JavaScript Functions
+// Available At: https://www.w3schools.com/js/js_functions.asp
+
+// Author: W3Schools
+// Version: Latest (2025)
+// Title: JavaScript Objects
+// Available At: https://www.w3schools.com/js/js_objects.asp
+
+// Author: W3Schools
+// Version: Latest (2025)
+// Title: TypeScript Interfaces
+// Available At: https://www.w3schools.com/typescript/typescript_interfaces.php
+
+// Author: W3Schools
+// Version: Latest (2025)
+// Title: TypeScript Types
+// Available At: https://www.w3schools.com/typescript/typescript_types.php
